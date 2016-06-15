8018faa34f3dd151a4ea96b073497c422c0da002
lucaswiser <lucas.adams@gmail.com>
2016-06-14 21:39:55 -0700

    added defualt config

---
+++ default_config.py
@@ -0,0 +1,8 @@
+class Config:
+    batch_size = 50
+    sent_len = 100
+    num_layers = 1
+    keep_prob = 0.5
+    vocab_size = None #Set when you load tok_map
+    state_size = 256
+    embed_size = 256
